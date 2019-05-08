cf32f37660a44bb1cd085f004912eb03838f2f66
matt-violet <mattviolet@gmail.com>
2019-05-08 16:29:34 -0700

    Implement front end React components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,164 @@
+// Build your UI using ReactJS and pre-compile your views using Babel
+// MUST place all of your React components into one file, app.jsx
+
+/* 
+F1 collects name, email, and password for account creation.
+F2 collects ship to address (line 1, line 2, city, state, zip code) and phone number.
+F3 collects credit card #, expiry date, CVV, and billing zip code.
+*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      home: true,
+      form1: false,
+      form2: false,
+      form3: false,
+      confirmation: false,
+      checkOutData: {
+        name: '',
+        email: '',
+        password: '',
+        address1: '',
+        address2: '',
+        city: '',
+        state: '',
+        zip: '',
+        phone: '',
+        creditCard: '',
+        expDate: '',
+        CVV: '',
+        billingZip: ''
+      }
+    };
+    this.displayForm1 = this.displayForm1.bind(this);
+  }
+
+  displayForm1(event) {
+    this.setState({
+      home: false,
+      form1: true,
+      form2: false,
+      form3: false,
+      confirmation: false
+    });
+    event.preventDefault();
+  }
+
+  displayForm2(event) {
+    this.setState({
+      home: false,
+      form1: false,
+      form2: true,
+      form3: false,
+      confirmation: false
+    });
+    event.preventDefault();
+  }
+
+  displayForm3(event) {
+    this.setState({
+      home: false,
+      form1: false,
+      form2: false,
+      form3: true,
+      confirmation: false
+    });
+    event.preventDefault();
+  }
+
+  displayConfirmation(event) {
+    this.setState({
+      home: false,
+      form1: false,
+      form2: false,
+      form3: false,
+      confirmation: true
+    });
+    event.preventDefault();
+  }
 
 
+
+  render() {
+    if (this.state.home === true) {
+    
+      return (
+        <div>
+          <h2 class='header'>Shopping List</h2>
+          <form onSubmit={(event) => this.displayForm1(event)}>
+            <input type='submit' value='Check Out'/>
+          </form>
+        </div>
+      );
+    }
+
+    if (this.state.form1 === true) {
+    
+      return (
+        <div>
+          <h2 class='header'>Create an Account</h2>
+          <form onSubmit={(event) => this.displayForm2(event)}>
+            Name: <input type='text'/><br></br>
+            Emal: <input type='text'/><br></br>
+            Password: <input type='text'/><br></br>
+            <input type='submit' value='Next'/>
+          </form>
+        </div>
+      );
+    }
+
+    if (this.state.form2 === true) {
+    
+      return (
+        <div>
+          <h2 class='header'>Enter Address</h2>
+          <form onSubmit={(event) => this.displayForm3(event)}>
+            Address: <input type='text'/><br></br>
+            Address: <input type='text'/><br></br>
+            City: <input type='text'/><br></br>
+            State: <input type='text'/><br></br>
+            Zip: <input type='text'/><br></br>
+            Phone: <input type='text'/><br></br>
+            <input type='submit' value='Next'/>
+          </form>
+        </div>
+      );
+    }
+
+    if (this.state.form3 === true) {
+    
+      return (
+        <div>
+          <h2 class='header'>Enter Credit Card</h2>
+          <form onSubmit={(event) => this.displayConfirmation(event)}>
+            Credit Card Number: <input type='text'/><br></br>
+            Expiry Date: <input type='text'/><br></br>
+            CVV: <input type='text'/><br></br>
+            Billing Zip Code: <input type='text'/><br></br>
+            <input type='submit' value='Next'/>
+          </form>
+        </div>
+      );
+    }  
+
+    if (this.state.confirmation === true) {
+    
+      return (
+        <div>
+          <h2 class='header'>Confirm Your Order</h2>
+          <form>
+            Order Summary: <br></br>
+            Item 1 <br></br>
+            Item 2 <br></br>
+            Item 3 <br></br>
+            <input type='submit' value='Purchase'/>
+          </form>
+        </div>
+      );
+    }  
+
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,16 @@
+// Use MongoDB or MySQL to store your user data
+// Build your Express app inside server.js
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('GET request to the homepage'))
+
+app.post('/', (req, res) => res.send('POST request to the homepage'))
+
+// serve index.html and associated files
+app.use('/', express.static('public'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
